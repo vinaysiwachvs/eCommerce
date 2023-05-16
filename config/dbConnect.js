0245003e65988d1781bcca9cd59ef44d76ada3ec
dramnay <deepanshu.ramnay@gmail.com>
2023-05-16 11:32:43 +0530

    database connection and disconnection

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,14 +1,22 @@
 const mongoose = require("mongoose");
 
 const initDB = () => {
-  mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Database connected successfully");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-}
+    mongoose
+        .connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            serverSelectionTimeoutMS: 5000,
+        })
+        .then(() => {
+            console.log("Database connected successfully");
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
 
-module.exports = { initDB };
+const disconnectDB = () => {
+    mongoose.disconnect();
+    console.log("Database disconnected successfully");
+};
+
+module.exports = { initDB, disconnectDB };
