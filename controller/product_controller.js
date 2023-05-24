3922a19ea170f3b7816bc2fac611a4094e9aee16
dramnay <deepanshu.ramnay@gmail.com>
2023-05-24 11:43:37 +0530

    added userId while creating review

--- controller/product_controller.js
+++ controller/product_controller.js
@@ -43,62 +43,56 @@ exports.getProductById = async(req, res) => {
     try {
         const id = req.params.id;
         const product = await productService.getProductById(id);
-        if (!product ) {
-        res.status(404).send("Product not found");
-        }else res.status(200).send(product);
-    }catch(error){
+        if (!product) {
+            res.status(404).send("Product not found");
+        } else res.status(200).send(product);
+    } catch (error) {
         console.log("error in getting product by id ", error);
         res.status(400).send({ message: error.message });
     }
 };
 
-
-
 exports.createReview = async(req, res) => {
     try {
         // const { productId } = req.params;
-        const  productId  = req.params.id;
+        const productId = req.params.id;
         const { comment, rating } = req.body;
-        
+
         const userId = req.loggedInUser._id;
-        const review = {
+
+        const result = await productService.createReview(
+            productId,
             userId,
             comment,
-            rating            
-        };
+            rating
+        );
 
-        const result = await productService.createReview(productId,review);
-
-
-        res.status(201).json({message: result});
+        res.status(201).json({ message: result });
     } catch (error) {
-        console.error("error occured in adding a review",error);
+        console.error("error occured in adding a review", error);
         res.status(400).json({ message: "Internal server error" });
     }
 };
 
-
-exports.getReviews = async (req,res) => {
-    try{
-
-        const  productId  = req.params.id;
+exports.getReviews = async(req, res) => {
+    try {
+        const productId = req.params.id;
         const product = await productService.getProductById(productId);
         console.log(product.reviews);
         return res.status(200).json(product.reviews);
-
-    }catch(error){
-        console.log("error in getting product ", error)
+    } catch (error) {
+        console.log("error in getting product ", error);
         res.status(400).send({ message: error.message });
     }
 };
 
-exports.deleteProduct = async (req,res) => {
-    try{
+exports.deleteProduct = async(req, res) => {
+    try {
         const productId = req.params.id;
-       const result= await productService.deleteProduct(productId);
-       res.status(201).send({ message: "result" });
-    }catch{
-        console.log("error in deleting product ", error)
+        const result = await productService.deleteProduct(productId);
+        res.status(201).send({ message: "result" });
+    } catch {
+        console.log("error in deleting product ", error);
         res.status(500).send({ message: error.message });
     }
-}
+};
--- service/product_service.js
+++ service/product_service.js
@@ -1,60 +1,61 @@
 // const AuthError = require("../errors/autherror");
 const Product = require("../model/product");
 
-const getAllProduct = async (page, limit) => {
-  const parsedPage = parseInt(page);
-  const parsedLimit = parseInt(limit);
+const getAllProduct = async(page, limit) => {
+    const parsedPage = parseInt(page);
+    const parsedLimit = parseInt(limit);
 
-  if (parsedPage < 0 || parsedLimit < 0)
-    throw new Error(`Please enter a positive value for page number or limit`);
+    if (parsedPage < 0 || parsedLimit < 0)
+        throw new Error(`Please enter a positive value for page number or limit`);
 
-  const skipIndex = (parsedPage - 1) * parsedLimit;
+    const skipIndex = (parsedPage - 1) * parsedLimit;
 
-  const products = await Product.aggregate([
-    {
-      $project: {
-        _id: 1,
-        name: 1,
-        icon: 1,
-        url: 1,
-        shortDescription: 1,
-        createdOn: 1,
-      },
-    },
-    { $skip: skipIndex },
-    { $limit: parsedLimit },
-  ]);
+    const products = await Product.aggregate([{
+            $project: {
+                _id: 1,
+                name: 1,
+                icon: 1,
+                url: 1,
+                shortDescription: 1,
+                createdOn: 1,
+            },
+        },
+        { $skip: skipIndex },
+        { $limit: parsedLimit },
+    ]);
 
-  if (!products[0]) throw new Error("Product not found");
+    if (!products[0]) throw new Error("Product not found");
 
-  return products;
+    return products;
 };
 
-const createProduct = async (product) => {
-  console.log("In create Product ", product);
-  await product.save();
-  return product.id;
+const createProduct = async(product) => {
+    console.log("In create Product ", product);
+    await product.save();
+    return product.id;
 };
 
-const getProductById = async (id) => {
-  const product = await Product.findById(id);
-  console.log(product);
-  return product;
+const getProductById = async(id) => {
+    const product = await Product.findById(id);
+    console.log(product);
+    return product;
 };
 
-const createReview = async (productId, review) => {
-    // Find the product by ID
-    const product = await Product.findById(productId);
-
-const createReview = async(productId, review) => {
+const createReview = async(productId, userId, comment, rating) => {
     try {
         // Find the product by ID
         const product = await Product.findById(productId);
-        
+
         if (!product) {
             throw new Error("Product not found");
-        } 
-        
+        }
+
+        const review = {
+            user: userId,
+            comment,
+            rating,
+        };
+
         // Add the review to the product's reviews array
         product.reviews.push(review);
 
@@ -64,44 +65,26 @@ const createReview = async(productId, review) => {
             (product.rating * (totalReviews - 1) + review.rating) / totalReviews;
         product.rating = averageRating;
 
-        await Product.updateOne(
-            { _id: productId },
-            { $set: product}
-          );
+        await Product.updateOne({ _id: productId }, { $set: product });
 
         return { message: "Review posted successfully" };
     } catch (error) {
         console.error(error);
         throw new Error("Failed to post review");
     }
-
-    // Add the review to the product's reviews array
-    product.reviews.push(review);
-
-    // Calculate the new average rating for the product
-    const totalReviews = product.reviews.length;
-    const averageRating =
-      (product.rating * (totalReviews - 1) + review.rating) / totalReviews;
-    product.rating = averageRating;
-
-    await Product.updateOne({ _id: productId }, { $set: product });
-
-    return "Review posted successfully";
 };
 
-const deleteProduct = async (productId) => {
-    try{
-        
-       const deletedProduct= await Product.findByIdAndRemove(productId);
-       if(!deletedProduct){
-        console.log("product not found");
-        return { message: "product do not exist" };
-       }
-       else  return { message: "product deleted successfully" };
-    } catch(error){
+const deleteProduct = async(productId) => {
+    try {
+        const deletedProduct = await Product.findByIdAndRemove(productId);
+        if (!deletedProduct) {
+            console.log("product not found");
+            return { message: "product do not exist" };
+        } else return { message: "product deleted successfully" };
+    } catch (error) {
         console.error(error);
         throw new Error("Failed to delete review");
     }
-}
+};
 
-module.exports = { createProduct, getProductById, getAllProduct,createReview};
+module.exports = { createProduct, getProductById, getAllProduct, createReview };
